dd287ba09b75fe11dd97c4fc8a90de50944363b9
BalazsDorottya <balazsdorottya22@gmail.com>
2026-05-11 17:57:31 +0300

    task 3 - test fail -10re frontend-en

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -102,6 +102,7 @@ public class PersonPageTests
     [TestCase(10, 5500)]
     [TestCase(0, 5000)]
     [TestCase(-5, 4750)]
+    [TestCase(-10, 4500)]
     public void Person_SalaryIncrease_ShouldIncrease(double percentage, double expectectedSalary)
     {
         // Arrange
